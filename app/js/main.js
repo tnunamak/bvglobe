083a952320e58023b184b1b44dc32b71ab67d4a6
Stephen Rugh <stephenrugh@gmail.com>
2015-06-23 17:55:19 -0500

    Remove jquery

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,39 @@
-$(function () {
-  if(!Detector.webgl){
-      Detector.addGetWebGLMessage();
-    } else {
-      var container = $('#container');
-      var globe = new DAT.Globe(container[0]);
 
-      console.log(globe);
-      var i, tweens = [];
+if(!Detector.webgl){
+  Detector.addGetWebGLMessage();
+} else {
+  var container = document.getElementById('container');
+  window.globe = new DAT.Globe(container);
 
-      var xhr;
-      TWEEN.start();
+  console.log(globe);
+  var i, tweens = [];
+
+  var xhr;
+  TWEEN.start();
 
 
-      xhr = new XMLHttpRequest();
-      xhr.open('GET', '/globe/fake.json', true);
-      xhr.onreadystatechange = function(e) {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            window.data = data;
-            changeData(0)
-            globe.animate();
-            document.body.style.backgroundImage = 'none'; // remove loading
-          }
-        }
-      };
-      xhr.send(null);
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', '/globe/data.json', true);
+  xhr.onreadystatechange = function(e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        window.data = data;
+        changeData(0);
+        globe.animate();
+        document.body.style.backgroundImage = 'none'; // remove loading
+      }
     }
-    window.changeData = function (i) {
-      globe.resetData();
-      globe.addData(window.data[i][1], {
-        format: 'magnitude',
-        name: window.data[i][0],
-        animated: true
-      });
-      globe.createPoints();
-      new TWEEN.Tween(globe).to({ time: i/3 },500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-    }
-})
+  };
+  xhr.send(null);
+}
+window.changeData = function (i) {
+  globe.addData(window.data[i][1], {
+    format: 'magnitude',
+    name: window.data[i][0],
+    animated: false
+  });
+  // new TWEEN.Tween(globe).to({ time: i/3 },500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+}
+
 
