1070783053a9363555972c217bc229176e2e63d3
Stephen Rugh <stephenrugh@gmail.com>
2015-06-23 16:02:51 -0500

    cleanup

--- app/js/main.js
+++ app/js/main.js
@@ -8,28 +8,6 @@ $(function () {
       console.log(globe);
       var i, tweens = [];
 
-      var settime = function(globe, t) {
-        return function() {
-          new TWEEN.Tween(globe).to({
-                time: t/3
-              },500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-          // var y = document.getElementById('year'+years[t]);
-          // if (y.getAttribute('class') === 'year active') {
-          //   return;
-          // }
-          // var yy = document.getElementsByClassName('year');
-          // for(i=0; i<yy.length; i++) {
-          //   yy[i].setAttribute('class','year');
-          // }
-          // y.setAttribute('class', 'year active');
-        };
-      };
-
-      // for(var i = 0; i<years.length; i++) {
-      //   var y = document.getElementById('year'+years[i]);
-      //   y.addEventListener('mouseover', settime(globe,i), false);
-      // }
-
       var xhr;
       TWEEN.start();
 
@@ -41,11 +19,7 @@ $(function () {
           if (xhr.status === 200) {
             var data = JSON.parse(xhr.responseText);
             window.data = data;
-            for (i=0;i<data.length;i++) {
-              globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
-            }
-            globe.createPoints();
-            settime(globe,0)();
+            changeData(0)
             globe.animate();
             document.body.style.backgroundImage = 'none'; // remove loading
           }
@@ -61,47 +35,7 @@ $(function () {
         animated: true
       });
       globe.createPoints();
-      // new TWEEN.Tween(globe).to({ time: i/3 },500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+      new TWEEN.Tween(globe).to({ time: i/3 },500).easing(TWEEN.Easing.Cubic.EaseOut).start();
     }
-// (function() {
-
-// var scene, camera, renderer;
-// var geometry, material, mesh;
-
-// init();
-// animate();
-
-// function init() {
-
-//     scene = new THREE.Scene();
-
-//     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-//     camera.position.z = 1000;
-
-//     geometry = new THREE.BoxGeometry( 200, 200, 200 );
-//     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-//     mesh = new THREE.Mesh( geometry, material );
-//     scene.add( mesh );
-
-//     renderer = new THREE.WebGLRenderer();
-//     renderer.setSize( window.innerWidth, window.innerHeight );
-
-//     document.body.appendChild( renderer.domElement );
-
-// }
-
-// function animate() {
-
-//     requestAnimationFrame( animate );
-
-//     mesh.rotation.x += 0.01;
-//     mesh.rotation.y += 0.02;
-
-//     renderer.render( scene, camera );
-
-// }
-
-// })();
 })
 
